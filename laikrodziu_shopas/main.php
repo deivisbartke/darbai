2a235d6d2b0321969b715447d5b63a49f883d368
LAPTOP-P41OBKJO\deivi <deivisbartke@gmail.com>
2018-04-11 17:56:03 +0300

    no message

--- laikrodziu_shopas/main.php
+++ laikrodziu_shopas/main.php
@@ -1,12 +1,27 @@
 <body>
 
-<h1>yo</h1>
+<div class="container-fluid">
+
+<div class="video-container">
+
+<video autoplay loop muted id="video-bg">
+  <source src="video/back.mp4" type="video/mp4">
+  </video>
+
+</div>
+
+    <div id="mySidenav" class="sidenav">
+      <img class="logo" src="foto/picture1.png" alt="">
+      <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+      <a href="#">About</a>
+      <a href="#">Product</a>
+      <a href="#">Reviews</a>
+      <a href="#">Contacts</a>
+    </div>
+        <span style="font-size:30px; color:orange ; cursor:pointer" onclick="openNav()">&#9776; Meniu</span>
 
 
 
 
 
-
-
-
-</body>
+</div>
